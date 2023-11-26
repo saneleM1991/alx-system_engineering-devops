2c1941365df23522264485c066b5a8a65bfdc367
saneleM1991 <91sanelem@gmail.com>
2023-11-26 19:32:40 +0200

    fix files

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# create a file in tmp folder
-
+# This manifest creates a file at /tmp
 file { '/tmp/school':
-  content =>'I love Puppet',
-  mode    => '0744',
-  owner   => 'www-dta',
-  group   => 'www-data',
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,5 @@
-# Kill process
-
-exec { 'pkill':
-  command  => 'pkill killmenow',
-  provider => 'shell',
+# Executes a bash command
+exec { 'kill':
+  command => 'pkill -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin']
 }
